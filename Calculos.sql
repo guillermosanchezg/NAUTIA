5cb63e462fa3cf4c1e6bc5e22a371982487047a3
GuillermoSanchez <guillermo.sanchezg@alumnos.upm.es>
2019-10-08 15:48:36 +0200

    Solucion de errores en modelo

--- Calculos.sql
+++ Calculos.sql
@@ -23,10 +23,10 @@ WHERE Community_idCommunity = 2;
 
 -- Sensación de seguridad en el asentamiento
 
-SELECT se1.Place_Time as "Lugar", COUNT(se2.idSE_SafetyPlace_has_Community) as "Nº Respustas afirmativas", se1.idSE_ComunitySafety
+SELECT se1.Place_Time as "Lugar", COUNT(se2.idSE_SafetyPlace_has_Community) as "Nº Respustas afirmativas", se2.Community_idCommunity
 FROM se_safetyplace se1, se_safetyplace_has_community se2
-WHERE se1.idSE_ComunitySafety  = se2.SE_SafetyPlace_idSE_ComunitySafety AND se2.Community_idCommunity = 1 AND se2.Answer = 'YES'
-ORDER BY se1.idSE_ComunitySafety;
+WHERE se1.idSE_ComunitySafety  = se2.SE_SafetyPlace_idSE_ComunitySafety AND se2.Answer = 'YES'
+GROUP BY se2.Community_idCommunity, se1.idSE_ComunitySafety;
 
 SELECT se1.Place_Time, COUNT(se2.idSE_SafetyPlace_has_Community)
 FROM se_safetyplace se1, se_safetyplace_has_community se2
@@ -34,7 +34,6 @@ WHERE se1.idSE_ComunitySafety  = se2.SE_SafetyPlace_idSE_ComunitySafety
 ORDER BY se1.idSE_ComunitySafety;
 
 -- Seguridad en letrinas
-SELECT * FROM se_safetylatrines;
 
 SELECT COUNT(*)
 FROM se_safetylatrines
@@ -52,13 +51,18 @@ SELECT (SELECT COUNT(sl.SeparatedBySex)*100/count(se_safetylatrines.idSE_Latrine
 				FROM se_safetylatrines sl
                 WHERE sl.SeparatedBySex = "YES") 
 FROM se_safetylatrines
-GROUP BY se_safetylatrines.SeparatedBySex;
+GROUP BY se_safetylatrines.Community_idCommunity;
 
-SELECT (SELECT COUNT(sl.Light)*100/count(se_safetylatrines.idSE_LatrinesBySex)
-				FROM se_safetylatrines sl
-                WHERE sl.Light = "YES")
+SELECT se_safetylatrines.Community_idCommunity, 
+		(SELECT COUNT(sl.Light)*100/COUNT(se_safetylatrines.idSE_LatrinesBySex)
+			FROM se_safetylatrines sl
+            WHERE sl.Light = "YES"
+            GROUP BY sl.Community_idCommunity)
 FROM se_safetylatrines
-GROUP BY se_safetylatrines.Light;
+GROUP BY se_safetylatrines.Community_idCommunity;
+
+SELECT * FROM se_safetylatrines;
+
 
 
 
