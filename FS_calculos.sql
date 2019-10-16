3e67ad93c4532ca4d560762e7c715db0e19d2fe5
GuillermoSanchez <guillermo.sanchezg@alumnos.upm.es>
2019-10-16 11:11:59 +0200

    Creación de FS_Calculos

---
+++ FS_calculos.sql
@@ -0,0 +1,14 @@
+-- ----------------------------------------------
+-- CÁLCULOS DERIVADOS DE LOS DATOS DE CONTEXTO --
+-- ----------------------------------------------
+
+-- ----------------------------------------------
+-- Datos de Seguridad Alimetaria	           --
+-- ----------------------------------------------
+
+-- Acesso a comida --
+
+-- Numero de personas con acceso a comida por tipo de comida
+
+SELECT *
+FROM FS_Foo
