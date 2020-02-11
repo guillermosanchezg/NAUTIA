dbd5abc1f161a809a4791c2bb5612d63528976e0
guillermosanchezg <guillesanchezgc@gmail.com>
2020-02-11 14:57:43 +0100

    avance en la T

--- DesarrolloPy/ScriptFinal.py
+++ DesarrolloPy/ScriptFinal.py
@@ -10,30 +10,31 @@ import os
 
 mainpath = "C:/Users/guill/Documents/Universidad/PlataformaRefugiados/NAUTIA/DesarrolloPy/DataSetOriginales"
 
-def dfFix(df,col1,col2):
+def dfFix(df,col1 = False,col2 = False):
     result = df.copy()
-    x = result.columns.get_loc(col1)
-    result.drop(result.columns[0:x],axis = 1, inplace = True)
-    y = result.columns.get_loc(col2)
-    result.drop(result.columns[y+1:],axis = 1, inplace = True)
+    if(col1):
+        x = result.columns.get_loc(col1)
+        result.drop(result.columns[0:x],axis = 1, inplace = True)
+    if(col2):
+        y = result.columns.get_loc(col2)
+        result.drop(result.columns[y:],axis = 1, inplace = True)
     return result
-
-    
+ 
 def concatDF(df1,df2):
     return  pd.concat([df1,df2],axis = 1)
 
 def mkCSV(df,fileName):
-    df.reset_index().to_csv('DataSetFinales/'+fileName,header = True, index=False)
+    df.to_csv('DataSetFinales/'+fileName,header = False, index=False) #Header e index a false para no mostrarlo en el csv
+  
     
 def getPath(mainpath,filename):
     return os.path.join(mainpath, filename)
 
 def fixBibliography(df):
-    df = dfFix(df,"GENERAL INFORMATION - COUNTRY LEVEL","Unnamed: 2")
+    df = dfFix(df,"GENERAL INFORMATION - COUNTRY LEVEL")
     df.columns = ['GeneralInfo', 'CommunityCountry', 'RefugeeCountry']
+    df.set_index('GeneralInfo', inplace = True)
     df = df.transpose()
-    df.columns = df.iloc[0]   #to set the real columns names
-    df = df.drop(df.index[0])
     return df
 
 
